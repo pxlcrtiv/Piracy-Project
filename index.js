b6523088a746eb850e50d62a6533ff2e64e449fa
emma-os <eeddie456@gmail.com>
2023-11-20 01:38:16 +0100

    Test complete
    Project passed tests and works as it should

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}],|:;<>.?/"; 
+let passUno=document.getElementById("pass1")
+let passDuos=document.getElementById("pass2")
+let inputElement = document.getElementById("input");
+let generatedPasswords = [];
+
+
+function generatePass() {
+    let password1 = "";
+    let password2 = "";
+    for (let i = 0; i < 16; i++) {
+        let randomUno = Math.floor(Math.random() * characters.length);
+        password1 += characters[randomUno];
+
+        let randomDuos = Math.floor(Math.random() * characters.length);
+        password2 += characters[randomDuos];
+    }
+    passUno.textContent = password1;
+    passDuos.textContent = password2;
+
+    generatedPasswords.push(password1);
+    generatedPasswords.push(password2);
+}
+function validateInput() {
+    let userInput = inputElement.value;
+    if (generatedPasswords.includes(userInput)) {
+        alert("Software Key is valid!");
+    } else {
+        alert("Pirated software detected!");
+    }
+}
+    
+    generatePass()
+
