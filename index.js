c66122cfd263105448e3a0db14c00dd5d0fca3c4
emma-os <eeddie456@gmail.com>
2023-11-20 02:51:31 +0100

    persistent
    another attempt to make it persistent

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}],|:;<>.?/"; 
-let passUno=document.getElementById("pass1")
-let passDuos=document.getElementById("pass2")
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}],|:;<>.?/";
+let passUno = document.getElementById("pass1");
+let passDuos = document.getElementById("pass2");
 let inputElement = document.getElementById("input");
 let generatedPasswords = [];
 
-
+// Function to generate passwords and store them in localStorage
 function generatePass() {
     let password1 = "";
     let password2 = "";
@@ -19,16 +18,24 @@ function generatePass() {
     passUno.textContent = password1;
     passDuos.textContent = password2;
 
+    // Store generated passwords in localStorage
     generatedPasswords.push(password1);
     generatedPasswords.push(password2);
+    localStorage.setItem("generatedPasswords", JSON.stringify(generatedPasswords));
 }
+
+// Function to validate user input
 function validateInput() {
     let userInput = inputElement.value;
-    if (generatedPasswords.includes(userInput)) {
+    // Retrieve generated passwords from localStorage
+    let storedPasswords = JSON.parse(localStorage.getItem("generatedPasswords")) || [];
+
+    if (storedPasswords.includes(userInput)) {
         alert("Software Key is valid!");
     } else {
         alert("Pirated software detected!");
     }
 }
-    
-    generatePass()
+
+// Call the generatePass function
+generatePass();
